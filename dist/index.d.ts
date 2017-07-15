9d58c2d384929cf1b38c9e07ac4259539edcd228
Phil Kunz <gitlab@philkunz.com>
2017-07-15 21:02:32 +0200

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,2 @@
-import { IAppJSON } from 'smartapp';
-import { IShipZoneData } from './servezone-interfaces.shipzone';
-/**
- * the socket function interface for adding an app to a servezone cluster
- */
-export interface ISocketAddApp {
-    shipzoneData: IShipZoneData;
-    appJSON: IAppJSON;
-}
-/**
- * the socket function interface for updating an app
- */
-export interface ISocketUpdateApp {
-    shipzoneData: IShipZoneData;
-    appJSON: IAppJSON;
-}
-/**
- * the socket function interface for checking for an app
- */
-export interface ISocketCheckApp {
-    shipzoneData: IShipZoneData;
-    appJSON: IAppJSON;
-}
+export * from './servezone-interfaces.servezone';
+export * from './servezone-interfaces.shipzone';
---
+++ dist/servezone-interfaces.servezone.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VydmV6b25lLWludGVyZmFjZXMuc2VydmV6b25lLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vdHMvc2VydmV6b25lLWludGVyZmFjZXMuc2VydmV6b25lLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==

72d55849342f1f49f4dfc2874cc0c7d6aa0381c2
Phil Kunz <gitlab@philkunz.com>
2019-08-20 12:25:05 +0200

    fix(core): update

--- ts/servezone-interfaces.configmanager.ts
+++ ts/servezone-interfaces.configmanager.ts
@@ -1,1 +1,18 @@
-export {};
+export interface IAppConfig {};
+
+export interface IServerConfig {
+  /**
+   * a list of debian packages to be installed
+   */
+  requiredPackages: string[];
+
+  /**
+   * needs reboot
+   */
+  reboot: boolean;
+
+  /**
+   * the time to check in again
+   */
+  checkInAgain: 600000;
+}

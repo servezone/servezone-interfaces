7a04336abe41cd431f3be24106b51ef5dfe89834
Phil Kunz <gitlab@philkunz.com>
2019-09-16 14:51:12 +0200

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,6 @@
+// cluster
+import * as clusterInterfaces from './servezone-interfaces.cluster';
+
 // config
 import * as configInterfaces from './servezone-interfaces.config';
 
@@ -11,6 +14,7 @@ import * as trafficInterfaces from './servezone-interfaces.traffic';
 import * as versionInterfaces from './servezone-interfaces.version';
 
 export {
+  clusterInterfaces as cluster,
   configInterfaces as config,
   requestInterfaces as request,
   trafficInterfaces as traffic,

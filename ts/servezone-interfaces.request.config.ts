ef58156ee27c7ced6b8a0ecfd7161fc74f966a96
Phil Kunz <gitlab@philkunz.com>
2019-09-17 10:50:17 +0200

    fix(core): update

--- ts/servezone-interfaces.request.config.ts
+++ ts/servezone-interfaces.request.config.ts
@@ -1,10 +1,24 @@
+import * as clusterInterfaces from './servezone-interfaces.cluster';
 import * as configInterfaces from './servezone-interfaces.config';
 
-export interface IRequest_Any_Cloudly_ConfigManager_Get {
-  method: 'get';
+export interface IRequest_Any_Cloudly_ConfigManager_GetServerConfig {
+  method: 'getServerConfig';
   request: {
-    type: configInterfaces.TConfigType;
+    clusterIdentifier: clusterInterfaces.IClusterIdentifier;
+  };
+  response: {
     configIdentifier: string;
+    configData:
+      | configInterfaces.IServerConfig
+      | configInterfaces.ICloreflowConfig
+      | configInterfaces.IServiceConfig;
+  };
+}
+
+export interface IRequest_Any_Cloudly_ConfigManager_GetClusterConfig {
+  method: 'getClusterConfig';
+  request: {
+    clusterIdentifier: clusterInterfaces.IClusterIdentifier;
   };
   response: {
     configIdentifier: string;

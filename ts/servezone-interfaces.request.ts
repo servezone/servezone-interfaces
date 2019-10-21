91b77f214a94b7c7abd06135045ea270110ec419
Phil Kunz <gitlab@philkunz.com>
2019-10-21 21:19:07 +0200

    fix(core): update

--- ts/servezone-interfaces.request.ts
+++ ts/servezone-interfaces.request.ts
@@ -2,6 +2,7 @@ import * as plugins from './servezone-interfaces.plugins';
 
 import * as configRequests from './servezone-interfaces.request.config';
 import * as identityRequests from './servezone-interfaces.request.identity';
+import * as networkRequests from './servezone-interfaces.request.network';
 import * as routingRequests from './servezone-interfaces.request.routing';
 import * as statusRequests from './servezone-interfaces.request.status';
 import * as versionRequests from './servezone-interfaces.request.version';
@@ -9,6 +10,7 @@ import * as versionRequests from './servezone-interfaces.request.version';
 export {
   configRequests as config,
   identityRequests as identity,
+  networkRequests as network,
   routingRequests as routing,
   statusRequests as status,
   versionRequests as version,

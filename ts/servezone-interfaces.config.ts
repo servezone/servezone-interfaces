a7fa28cb8adb812e968fa05162cab89303d66356
Phil Kunz <gitlab@philkunz.com>
2019-11-19 22:55:02 +0000

    fix(core): update

--- ts/servezone-interfaces.config.ts
+++ ts/servezone-interfaces.config.ts
@@ -1,3 +1,7 @@
+/* ==========
+This file exports all config interfaces for easier access under module.config.[anyconfig]
+// ========== */ 
+
 export * from './servezone-interfaces.config.cluster';
 export * from './servezone-interfaces.config.server';
 export * from './servezone-interfaces.config.service';

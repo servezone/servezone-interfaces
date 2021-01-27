62d76a2bc99bc95ae0155854c93e0bef079fb8da
Phil Kunz <gitlab@philkunz.com>
2021-01-27 21:06:15 +0000

    fix(core): update

--- ts/request/loint-cloudly.request.identity.ts
+++ ts/request/loint-cloudly.request.identity.ts
@@ -7,8 +7,8 @@ import * as clusterInterfaces from '../loint-cloudly.cluster';
 /**
  * get the identity that then will be used to get the config
  */
-export interface IRequest_Any_Cloudly_CoreflowManager_Identity {
-  method: 'identity';
+export interface IRequest_Any_Cloudly_CoreflowManager_GetIdentitybyJumpCode {
+  method: 'getIdentitybyJumpCode';
   request: {
     jumpCode: string;
   };
